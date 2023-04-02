ff558e793257a0d61e84fa16041f2c04070f0015
chshji666666 <chshji666666@163.com>
2023-04-02 10:56:06 +0800

    cc

--- src/hooks/useSubmit.ts
+++ src/hooks/useSubmit.ts
@@ -77,11 +77,13 @@ const useSubmit = () => {
     const handleSubmit = async () => {
         const usercountres = await handlusecountres();
         if (!usercountres) {
+            alert('key 不存在或者输入错误');
             throw new Error(t('key 不存在或者输入错误') as string);
+            
         }
         if (usercountres.usecount <= 0) {
+            alert('key 次数不足，请增加次数');
             throw new Error('key 次数不足，请增加次数');
-
         }
 
     const chats = useStore.getState().chats;
