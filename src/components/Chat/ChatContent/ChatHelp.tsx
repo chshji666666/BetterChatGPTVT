051a310d39257eccebe3699c3ee6523aab98f8e4
chshji666666 <chshji666666@163.com>
2023-04-03 10:31:40 +0800

    cc

---
+++ src/components/Chat/ChatContent/ChatHelp.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import useStore from '@store/store';
+
+import { ChatInterface } from '@type/chat';
+
+import TickIcon from '@icon/TickIcon';
+
+
+return (
+    <>
+        <button
+            className='btn btn-neutral'
+            onClick={() => {
+                window.open('http://www.promptcan.com/help/view10.html', '_blank');
+            }}
+        >
+            {t('使用帮助')}
+        </button>
+    </>
+);
+
+export default ChatHelp;
