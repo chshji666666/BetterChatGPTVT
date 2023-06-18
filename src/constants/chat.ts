02fc944b7237d7888ed6392de00ca505be015a80
chshji666666 <chshji666666@163.com>
2023-06-18 10:37:14 +0800

    增加gpt-3.5-turbo-16k
    增加gpt-3.5-turbo-16k，gpt-4-32k-0613

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -15,7 +15,8 @@ Carefully heed the user's instructions.
 Respond using Markdown.`;
 
 export const modelOptions: ModelOptions[] = [
-  'gpt-3.5-turbo',
+    'gpt-3.5-turbo',
+    'gpt-3.5-turbo-16k',
   'gpt-4',
   'gpt-4-32k',
   // 'gpt-3.5-turbo-0301',
@@ -27,20 +28,45 @@ export const defaultModel = 'gpt-3.5-turbo';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 4096,
-  'gpt-3.5-turbo-0301': 4096,
+    'gpt-3.5-turbo-0301': 4096,
+    'gpt-3.5-turbo-0613': 4096,
+    'gpt-3.5-turbo-16k': 16384,
+    'gpt-3.5-turbo-16k-0613': 16384,
   'gpt-4': 8192,
-  'gpt-4-0314': 8192,
+    'gpt-4-0314': 8192,
+    'gpt-4-0613': 8192,
   'gpt-4-32k': 32768,
-  'gpt-4-32k-0314': 32768,
+    'gpt-4-32k-0314': 32768,
+    'gpt-4-32k-0613': 32768,
 };
 
 export const modelCost = {
-  'gpt-3.5-turbo': { price: 0.002, unit: 1000 },
-  'gpt-3.5-turbo-0301': { price: 0.002, unit: 1000 },
+    'gpt-3.5-turbo': { price: 0.0015, unit: 1000 },
+    'gpt-3.5-turbo-0301': { price: 0.0015, unit: 1000 },
+    'gpt-3.5-turbo-0613': {
+        prompt: { price: 0.0015, unit: 1000 },
+        completion: { price: 0.002, unit: 1000 },
+    },
+    'gpt-3.5-turbo-16k': {
+        prompt: { price: 0.003, unit: 1000 },
+        completion: { price: 0.004, unit: 1000 },
+    },
+    'gpt-3.5-turbo-16k-0613': {
+        prompt: { price: 0.003, unit: 1000 },
+        completion: { price: 0.004, unit: 1000 },
+    },
   'gpt-4': { price: 0.03, unit: 1000 },
-  'gpt-4-0314': { price: 0.03, unit: 1000 },
+    'gpt-4-0314': { price: 0.03, unit: 1000 },
+    'gpt-4-0613': {
+        prompt: { price: 0.03, unit: 1000 },
+        completion: { price: 0.06, unit: 1000 },
+    },
   'gpt-4-32k': { price: 0.06, unit: 1000 },
-  'gpt-4-32k-0314': { price: 0.06, unit: 1000 },
+    'gpt-4-32k-0314': { price: 0.06, unit: 1000 },
+    'gpt-4-32k-0613': {
+        prompt: { price: 0.06, unit: 1000 },
+        completion: { price: 0.12, unit: 1000 },
+    },
 };
 
 export const defaultUserMaxToken = 4000;
