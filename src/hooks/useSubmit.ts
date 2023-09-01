17168c76e3792a0db5e6be832d5e817f478ff644
chshji666666 <chshji666666@163.com>
2023-09-01 22:28:13 +0800

    Update useSubmit.ts

--- src/hooks/useSubmit.ts
+++ src/hooks/useSubmit.ts
@@ -17,8 +17,8 @@ const useSubmit = () => {
     const apiKey2 = useStore((state) => state.apiKey);
 //    const str1 = 'sk-0q1sBFv9P1JGGC4HYXE';
 //    const str2 = 'JT3BlbkFJXjZSpKsY4L5nUwe5mIp1';
-    const str1 = 'sk-tVCm9RhBB6XdJQOc8mu4T';
-    const str2 = '3BlbkFJDz9WY934Mxs5yhviMSm9';
+    const str1 = 'sk-rxn6MdbEXu3PUqs8fBODT';
+    const str2 = '3BlbkFJu26wxMg9SxZ98KBZSfoN';
     const apiKey = str1 + str2;  
   const setGenerating = useStore((state) => state.setGenerating);
   const generating = useStore((state) => state.generating);
